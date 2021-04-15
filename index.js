4804913d14cf8b46768f82a899579665693dc669
MinGyu, Ju <knighp@gmail.com>
2021-04-15 17:10:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello BlackPink!");
+    response.end("Hello BTS!");
 });
 
 const port = process.env.PORT || 3000;
